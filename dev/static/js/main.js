3f649a63f141e4e7a8da829a0c0c777370100f92
Oleg Tsyganov <TsyganovDev@gmail.com>
2020-11-24 22:39:08 +0400

    Реализовал шапку и выпадающее меню для мобильной версии

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,22 +1,21 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    const sandwichToggle = function () {
-        // Выбираем элементы, которые нам нужны. В примере мы ищем элементы с классом "sandwich"
-        let sandwichElements = document.querySelectorAll('.sandwich');
-        // Проходим циклом по всем эдементам и на каждый элемент вешаем слушателя, который по клику будет переключать класс
-        sandwichElements.forEach((item) => {
-            item.addEventListener('click', showSandwichTarget);
-        });
-    
-        function showSandwichTarget() {
-            let targetId = this.getAttribute('data-target-id'),
-                targetClassToggle = this.getAttribute('data-target-class-toggle');
-            this.classList.toggle('is-active');
-            if (targetId && targetClassToggle) {
-                document.getElementById(targetId).classList.toggle(targetClassToggle);
-            }
-        }
-    };
-    sandwichToggle();
+  const sandwichToggle = function() {
+    let sandwichElements = document.querySelectorAll('.sandwich');
+    sandwichElements.forEach((item) => {
+      item.addEventListener('click', showSandwichTarget);
+    });
+
+    function showSandwichTarget() {
+      let targetId = this.getAttribute('data-target-id');
+      let targetClassToggle = this.getAttribute('data-target-class-toggle');
+      this.classList.toggle('sandwich-open');
+      document.body.classList.toggle('overflow-hidden');
+      if (targetId && targetClassToggle) {
+        document.getElementById(targetId).classList.toggle(targetClassToggle);
+      }
+    }
+  };
+  sandwichToggle();
 })();
