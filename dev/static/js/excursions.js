e55d6428deecebf77ce3e8315db5a74426532f44
redsmoke19 <44342965+redsmoke19@users.noreply.github.com>
2020-12-03 15:29:09 +0400

    Адаптировал страницу с экскурсиями, реализовал модальные окна экскурсий, фикс баков и недочетов в процессе адаптации

--- dev/static/js/excursions.js
+++ dev/static/js/excursions.js
@@ -1,5 +1,6 @@
 (function() {
   'use strict';
+  let wrapper = document.querySelector('.wrapper');
   //Sliders
   const sliders = function() {
     const excursionCanyonSlider = new Swiper('.excursions__slider--canyon', {
@@ -7,8 +8,8 @@
       preventClicks: true,
       preventClicksPropagation: true,
       navigation: {
-        nextEl: '.excursions__slider--canyon .excursions__slider-button--next',
-        prevEl: '.excursions__slider--canyon .excursions__slider-button--prev',
+        nextEl: '.excursions__item--canyon .excursions__slider-button--next',
+        prevEl: '.excursions__item--canyon .excursions__slider-button--prev',
       },
     });
 
@@ -17,8 +18,8 @@
       preventClicks: true,
       preventClicksPropagation: true,
       navigation: {
-        nextEl: '.excursions__slider--master .excursions__slider-button--next',
-        prevEl: '.excursions__slider--master .excursions__slider-button--prev',
+        nextEl: '.excursions__item--master .excursions__slider-button--next',
+        prevEl: '.excursions__item--master .excursions__slider-button--prev',
       },
     });
 
@@ -27,10 +28,75 @@
       preventClicks: true,
       preventClicksPropagation: true,
       navigation: {
-        nextEl: '.excursions__slider--under-ruskeala .excursions__slider-button--next',
-        prevEl: '.excursions__slider--under-ruskeala .excursions__slider-button--prev',
+        nextEl: '.excursions__item--under-ruskeala .excursions__slider-button--next',
+        prevEl: '.excursions__item--under-ruskeala .excursions__slider-button--prev',
       },
     });
   };
   sliders();
+
+  // Open Close Modal
+  const modalToggle = function() {
+    let buyTicketButtons = document.querySelectorAll('.excursions__buy');
+    let buyTicketModal = document.querySelector('.excursions-ticket');
+    let buyTicketModalClose = document.querySelector('.excursions-ticket__close');
+    let ticketTitle = document.querySelector('.excursions-ticket__title');
+
+    let modalOpen = function(element, className, title) {
+      document.body.classList.add('overflow-hidden');
+      wrapper.classList.add('overflow-hidden');
+      element.classList.add(className);
+    }
+    let modalClose = function(element, className) {
+      document.body.classList.remove('overflow-hidden');
+      wrapper.classList.remove('overflow-hidden');
+      element.classList.remove(className);
+    }
+
+    buyTicketButtons.forEach(item => {
+      item.addEventListener('click', () => {
+        ticketTitle.textContent = `"${item.dataset.excursionName}"`;
+        modalOpen(buyTicketModal, 'excursions-ticket--open');
+      });
+    });
+    buyTicketModalClose.addEventListener('click', () => {
+      modalClose(buyTicketModal, 'excursions-ticket--open');
+    })
+  };
+  modalToggle();
+
+  //DatePicker
+  const datePickers = function() {
+    let currentDate = new Date();
+    let ticketDate = document.querySelector('.excursions-ticket__selected-date');
+    let customDays = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+    let customMonth = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
+    ticketDate.textContent = currentDate.toLocaleString('ru', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+
+
+    const enterTicketDatapicker = datepicker('.excursions-ticket__datepicker', {
+      formatter: (input, date, instance) => {
+        input.value = date.toLocaleDateString();
+      },
+      onSelect: (instance, date) => {
+        ticketDate.textContent = date.toLocaleString('ru', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        });
+      },
+      startDay: 1,
+      customDays: customDays,
+      customMonths: customMonth,
+      overlayButton: 'Выбрать',
+      overlayPlaceholder: 'Введите год',
+      dateSelected: new Date(),
+    });
+  }
+  datePickers();
 })();
