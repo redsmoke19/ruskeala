6a6530fda9826499baf6264d5501219e65d9e5cd
Oleg Tsyganov <TsyganovDev@gmail.com>
2020-12-07 23:44:10 +0400

    Сверстал и адаптировал меню ресторана, адаптировал hero на страницы Гостиница

---
+++ dev/static/js/where-to-eat.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  let menuModal = document.querySelector('.eat-menu');
+  let menuButtons = document.querySelectorAll('.js-eat-menu-open');
+  let closeMenuModal = document.querySelector('.eat-menu__close');
+  menuButtons.forEach(item => {
+    item.addEventListener('click', () => {
+      window.modalOpen(menuModal, 'eat-menu--open', true);
+    });
+  });
+  closeMenuModal.addEventListener('click', () => {
+    window.modalClose(menuModal, 'eat-menu--open');
+  });
+})();
