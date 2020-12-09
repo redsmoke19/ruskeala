04f5ac7728161f47eeb24fdea2333e270cdd6309
redsmoke19 <44342965+redsmoke19@users.noreply.github.com>
2020-12-09 16:06:51 +0400

    Реализовал верстку страницы Туры

--- dev/static/js/tours.js
+++ dev/static/js/tours.js
@@ -11,4 +11,34 @@
     },
   });
 
+  const toursPearlSlider = new Swiper('.tours-park__slider--pearl', {
+    direction: 'horizontal',
+    preventClicks: true,
+    preventClicksPropagation: true,
+    navigation: {
+      nextEl: '.tours-park__slider--pearl .tours-park__slider-button--next',
+      prevEl: '.tours-park__slider--pearl .tours-park__slider-button--prev',
+    },
+  });
+
+  const toursPearlDaySlider = new Swiper('.tours-park__slider--pearl-day', {
+    direction: 'horizontal',
+    preventClicks: true,
+    preventClicksPropagation: true,
+    navigation: {
+      nextEl: '.tours-park__slider--pearl-day .tours-park__slider-button--next',
+      prevEl: '.tours-park__slider--pearl-day .tours-park__slider-button--prev',
+    },
+  });
+
+  const toursMiraclesSlider = new Swiper('.tours-park__slider--miracles', {
+    direction: 'horizontal',
+    preventClicks: true,
+    preventClicksPropagation: true,
+    navigation: {
+      nextEl: '.tours-park__slider--miracles .tours-park__slider-button--next',
+      prevEl: '.tours-park__slider--miracles .tours-park__slider-button--prev',
+    },
+  });
+
 })();
