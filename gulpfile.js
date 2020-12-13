2a81bae2735092e744d3f8b58e373903fc9e8ea8
Oleg Tsyganov <TsyganovDev@gmail.com>
2020-12-13 18:15:11 +0400

    Добавил Critical CSS

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,22 @@ const spriteSVG = require('./gulp/tasks/spriteSVG');
 const serve = require('./gulp/tasks/serve');
 const spritePNG = require('./gulp/tasks/spritePNG');
 
+const critical = require('critical').stream;
+
+// Generate & Inline Critical-path CSS
+gulp.task('critical', () => {
+  return gulp
+    .src('dist/*.html')
+    .pipe(
+      critical({
+        base: 'dist/',
+        inline: true,
+        css: ['dist/static/css/styles.css'],
+      })
+    )
+    .pipe(gulp.dest('dist'));
+});
+
 const dev = gulp.parallel(pug2html, script, vendors, styles, imageMinify, spriteSVG, spritePNG, fonts, imageWebP);
 
 exports.default = gulp.series(
